bf36713f5f07fae2c6570ae2db3dedaa9e41b047
Brendan Kennedy <brendan.fj.kennedy@gmail.com>
2021-09-22 10:22:11 -0400

    Airlines created  at 1022  EST

--- HTMLandJS.js
+++ HTMLandJS.js
@@ -1,9 +1,25 @@
-window.addEventListener("click", function() {
-    alert("You clicked on the page!");
-  });
+var button = document.querySelector("button");
 
-  var button = document.querySelector("button");
-
-  button.addEventListener("click", function(){
+var clickAlert = function() {
     alert("SOMEONE CLICKED THE BUTTON!!");
-  });
+}
+ 
+
+button.addEventListener("click", clickAlert);
+
+var clickCount = 0
+
+var logText = function(event) {
+    console.log(event.target.textContent)
+    button.innerHTML = clickCount += 1;
+  }
+  
+  window.addEventListener("click", logText);
+
+  var div = document.querySelector("div");
+
+  var logText = function(event) {
+    console.log(event.target.textContent);
+  }
+  
+  div.addEventListener("click", logText);
